cb48bdc8663215d7f26b0499afaa0996ebded891
shouts77 <shouts98@gmail.com>
2021-08-21 01:42:54 +0000

    modified Home componetn padding

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,12 @@ function Home() {
       <Container maxWidth="lg">
       <div>
       <Typography variant="h4">
-        React D3.js Visualization
+        데이터 시각화 연습을 위한 앱
       </Typography>
       <br />
       </div>
       <Divider variant="fullWidth" />
-      <div style={{ padding: 20 }}>
+      <div style={{ paddingTop: 20, paddingBottom: 20 }}>
       <Typography paragraph>
          React와 D3.js로 데이터 시각화를 연습하기 위해 만든 앱입니다.
        </Typography>
