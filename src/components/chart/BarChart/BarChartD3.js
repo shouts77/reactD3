899039e472cdc40ac0b2b1563db501ac49e83eb6
shouts77 <shouts98@gmail.com>
2021-08-22 09:59:34 +0000

    fixed hook warning

--- src/components/chart/BarChart/BarChartD3.js
+++ src/components/chart/BarChart/BarChartD3.js
@@ -1,46 +1,43 @@
-import React from "react";
-import { useD3 } from '/home/node/react-d3/src/hooks/useD3';
+import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-const width = 500;
-const height = 500;
-const adjmargin = 200;
+export default function BarChartD3({data}) {
+  const ref = useRef();
 
+  useEffect(() => {
 
-function BarChartD3({data}) {
-  const ref = useD3(
-    (svg) => {
+    const width = 500;
+    const height = 500;
+    const adjmargin = 200;
+    const color = "steelblue";
+    const margin = ({top: 10, right: 30, bottom: 30, left: 30})
 
-      const color = "steelblue";
-      const margin = ({top: 10, right: 30, bottom: 30, left: 30})
+    const x = d3.scaleBand()
+                .domain(d3.range(data.length))
+                .range([margin.left, width - margin.right])
+                .padding(0.1)
 
-      const x = d3.scaleBand()
-                  .domain(d3.range(data.length))
-                  .range([margin.left, width - margin.right])
-                  .padding(0.1)
+    const y = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.value)]).nice()
+                .range([height - margin.bottom - adjmargin, margin.top])
 
-      const y = d3.scaleLinear()
-                  .domain([0, d3.max(data, d => d.value)]).nice()
-                  .range([height - margin.bottom - adjmargin, margin.top])
+    const xAxis = g => g.attr("transform", `translate(0,${height - margin.bottom - adjmargin})`)
+                        .call(d3.axisBottom(x)
+                        .tickFormat(i => data[i].name)
+                        .tickSizeOuter(0))
 
-      const xAxis = g => g.attr("transform", `translate(0,${height - margin.bottom - adjmargin})`)
-                          .call(d3.axisBottom(x)
-                          .tickFormat(i => data[i].name)
-                          .tickSizeOuter(0))
+    const yAxis = g => g.attr("transform", `translate(${margin.left},0)`)
+                        .call(d3.axisLeft(y).ticks(null, "%"))
+                        .call(g => g.select(".domain").remove())
+                        .call(g => g.append("text")
+                                    .attr("x", -margin.left)
+                                    .attr("y", 10)
+                                    .attr("fill", "currentColor")
+                                    .attr("text-anchor", "start")
+                                    .text(data.y))
 
-      const yAxis = g => g.attr("transform", `translate(${margin.left},0)`)
-                          .call(d3.axisLeft(y).ticks(null, "%"))
-                          .call(g => g.select(".domain").remove())
-                          .call(g => g.append("text")
-                                      .attr("x", -margin.left)
-                                      .attr("y", 10)
-                                      .attr("fill", "currentColor")
-                                      .attr("text-anchor", "start")
-                                      .text(data.y))
-
-      // const svg = d3.select(ref.current)
-      //               .append("svg")
-      //               .attr("viewBox", [0, 0, width, height]);
+      const svg = d3.select(ref.current)
+                    .attr("viewBox", [0, 0, width, height]);
 
       svg.select(".plot-area")
          .append("g")
@@ -56,27 +53,22 @@ function BarChartD3({data}) {
       svg.select(".x-axis").append("g").call(xAxis);
       svg.select(".x-axis").append("g").call(yAxis);
       
-      },
-      [data.length]
+      }
     );
-      
-      return (
-        <>
-          <svg
-            ref={ref}
-            viewBox={`0 0 ${height} ${width}`}
-            style={{
-              height: "100%",
-              marginRight: "0px",
-              marginLeft: "0px",
-            }}
-          >
-            <g className="plot-area" />
-            <g className="x-axis" />
-            <g className="y-axis" />
-          </svg>
+    return (
+    <>
+      <svg
+        ref={ref}
+        style={{
+          height: "100%",
+          marginRight: "0px",
+          marginLeft: "0px",
+        }}
+      >
+        <g className="plot-area" />
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
         </>
       );
 }
-
-export default BarChartD3;
--- src/components/chart/ScatterChart/ScatterChartD3.js
+++ src/components/chart/ScatterChart/ScatterChartD3.js
@@ -1,69 +1,62 @@
 import React, { useEffect, useRef } from "react";
-import { useD3 } from '/home/node/react-d3/src/hooks/useD3';
 import * as d3 from "d3";
 
-const width = 300;
-const height = 300;
-const adjmargin = 100;
+export default function ScatterChartD3({data}) {
+  const ref = useRef();
 
-
-function ScatterChartD3({data}) {
-  const ref = useD3(
-    (svg) => {
-      const color = "steelblue";
-      const margin = { top: 30, right: 30, bottom: 30, left: 30 };
+  useEffect(() => {
+    const width = 400;
+    const height = 400;
+    const adjmargin = 100;
+    const color = "steelblue";
+    const margin = { top: 30, right: 30, bottom: 30, left: 30 };
         
-      // const svg = d3.select(ref.current)
-      //               .append("svg")
-      //               .attr("viewBox", [0, 0, width, height]);
+    const svg = d3.select(ref.current)
+                  .attr("viewBox", [0, 0, width, height]);
 
-      const x = d3.scaleLinear()
-                  .domain([0, d3.max(data, d => d.x)])
-                  .range([margin.left, width - margin.right - adjmargin]);
+    const x = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.x)])
+                .range([margin.left, width - margin.right - adjmargin]);
 
-      svg.select(".x-axis")
-         .append("g")
-         .attr("transform", `translate(0,${height - margin.bottom - adjmargin})`)
-         .call(d3.axisBottom(x));
+    svg.select(".x-axis")
+       .append("g")
+       .attr("transform", `translate(0,${height - margin.bottom - adjmargin})`)
+       .call(d3.axisBottom(x));
 
-      const y = d3.scaleLinear()
-                  .domain([0, d3.max(data, d => d.y)])
-                  .range([height - margin.bottom - adjmargin, margin.top]);
+    const y = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.y)])
+                .range([height - margin.bottom - adjmargin, margin.top]);
 
-      svg.select(".y-axis")
-         .append("g")
-         .attr("transform", `translate(${margin.left},0)`)
-         .call(d3.axisLeft(y));
+    svg.select(".y-axis")
+       .append("g")
+       .attr("transform", `translate(${margin.left},0)`)
+       .call(d3.axisLeft(y));
 
-      svg.select(".plot-area")
-         .selectAll("circle")
-         .data(data)
-         .join("circle")
-              .attr("fill", color)
-              .attr("cx", (d) => x(d.x))
-              .attr("cy", (d) => y(d.y))
-              .attr("r", 3);
-        },
-        [data.length]
-      );
+    svg.select(".plot-area")
+       .selectAll("circle")
+       .data(data)
+       .join("circle")
+            .attr("fill", color)
+            .attr("cx", (d) => x(d.x))
+            .attr("cy", (d) => y(d.y))
+            .attr("r", 3);
+    }
+  );
 
   return (
     <>
-    <svg
-      ref={ref}
-      viewBox={`0 0 ${height} ${width}`}
-      style={{
-        height: "100%",
-        marginRight: "0px",
-        marginLeft: "0px",
-      }}
-    >
-      <g className="plot-area" />
-      <g className="x-axis" />
-      <g className="y-axis" />
-    </svg>
-  </>
-  );
+      <svg
+        ref={ref}
+        style={{
+          height: "100%",
+          marginRight: "0px",
+          marginLeft: "0px",
+        }}
+      >
+        <g className="plot-area" />
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+        </>
+      );
 }
-
-export default ScatterChartD3;
