f263078d3f60b5dcb5192a29196e514e93b62854
shouts <shouts77@users.noreply.github.com>
2022-10-18 10:45:35 +0900

    Update PachinkoSimD3.js

--- src/components/chart/PachinkoSim/PachinkoSimD3.js
+++ src/components/chart/PachinkoSim/PachinkoSimD3.js
@@ -5,6 +5,8 @@ export default function PachinkoSimD3() {
   const ref = useRef();
 
   useEffect(() => {
+    
+    function* gen() {
     //base setting
     const width = 300;
     const height = 300;
@@ -70,6 +72,8 @@ export default function PachinkoSimD3() {
         .duration(750)
         // .ease(d3.easeBounce)
         .attr('cy', cy);
+    }
+      yield svg.node()
     }
   }, []);
 
